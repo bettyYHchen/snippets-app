54ea0d1ef8e4f89c0c8b4139913d97c76cbf5bfb
Betty Chen <betty_c@outlook.com>
2014-10-04 17:36:59 -0400

    create snippet.py

--- snippet.py
+++ snippet.py
@@ -1,11 +1,33 @@
 import logging
 import csv
+import argparse
+import sys
+
+def make_parser():
+	"""Construct the command line parse"""
+	logging.info("Constructing parser")
+	description="Store and retrieve snippets of text"
+	parser=argparse.ArgumentParser(description=description)
+	#the add_subparsers method allow us to add multiple subparser
+	#For subparser, I mean the subset of the arguments that the program can still run
+
+	subparsers = parser.add_subparsers(help="Available commands")
+	# Subparser for the put command
+	logging.debug("Constructing put subparser")
+	#Initialize a new subparser (subparser is same as parser except that it allow partial arguments)
+	put_parser = subparsers.add_parser("put",help="Store a snippet")
+	#Add argument of the parser
+	put_parser.add_argument("name", help="The name of the snippet")
+	put_parser.add_argument("snippet", help="The snippet text")
+	put_parser.add_argument("filename", default="snippets.csv",nargs="?"
+		                    help="The snippet filename")
+
+	return parser
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
 def put(name,snippet,filename):
-	""" Store a snippet with a assoicated name in the CSV file """
     logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
     logging.debug("Opening file")
     with open(filename,"a") as f:
@@ -14,3 +36,18 @@ def put(name,snippet,filename):
     	writer.writerow([name, snippet])
     logging.debug("Write sucessful")
     return name, snippet
+
+def main():
+	""" Main function """
+	logging.info("Starting snippet")
+	parser=make_parser() #make_parser create the command line parser object
+	arguments=parser.parse_args(sys.argv[1:]) #the parse_args method of the perser
+	#object, passing in all of the command line argument except the first, coz the 
+	#first one always contains the name of our program
+	#It returns a Namespace object, and we want to convert it to a dictionary object
+	#arguments = vars(arguments) #Convert to dictionary object
+
+
+if __name__ == "__main__":
+	main()
+
